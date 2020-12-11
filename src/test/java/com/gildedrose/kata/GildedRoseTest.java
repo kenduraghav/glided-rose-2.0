488b891d5b543a7fe2e19f4b20df4b07ab9794e1
kenduraghav <kenduraghav@gmail.com>
2020-12-12 01:29:14 +0800

    Extracted Strings, numbers to constant - refactoring

--- src/test/java/com/gildedrose/kata/GildedRoseTest.java
+++ src/test/java/com/gildedrose/kata/GildedRoseTest.java
@@ -40,67 +40,67 @@ class GildedRoseTest {
 
 	@Test
 	void agedBrieIncreaseQuality() {
-		Item item = createAndUpdate("Aged Brie", 15, 25);
+		Item item = createAndUpdate(GildedRose.AGED_BRIE, 15, 25);
 		assertEquals(26, item.quality);
 	}
 
 	@Test
-	void qualityIsNeverMoreThan50() {
+	void qualityIsNeverMoreThanMaximum() {
 		Item item = createAndUpdate("foo", 15, 52);
 		assertEquals(51, item.quality);
 
-		item = createAndUpdate("Aged Brie", 15, 50);
-		assertEquals(50, item.quality);
+		item = createAndUpdate(GildedRose.AGED_BRIE, 15, GildedRose.MAXIMUM_QUALITY);
+		assertEquals(GildedRose.MAXIMUM_QUALITY, item.quality);
 	}
 
 	@Test
 	void sulfurasNeverToBeSoldOrDecreaseQuality() {
-		Item item = createAndUpdate("Sulfuras, Hand of Ragnaros", 1, 25);
+		Item item = createAndUpdate(GildedRose.SULFURAS, 1, 25);
 		assertEquals(1, item.sellIn);
 		assertEquals(25, item.quality);
 	}
 
 	@Test
 	void backStagePassesIncreaseInQuality() {
-		Item item = createAndUpdate("Backstage passes to a TAFKAL80ETC concert", 15, 25);
+		Item item = createAndUpdate(GildedRose.BACKSTAGE_PASSES , 15, 25);
 		assertEquals(26, item.quality);
 	}
 
 	@Test
 	void backstagePassesIncreaseBy2() {
-		Item item = createAndUpdate("Backstage passes to a TAFKAL80ETC concert", 10, 25);
+		Item item = createAndUpdate(GildedRose.BACKSTAGE_PASSES , 10, 25);
 		assertEquals(27, item.quality);
 	}
 
 	@Test
 	void backstagePassesIncreaseBy3() {
-		Item item = createAndUpdate("Backstage passes to a TAFKAL80ETC concert", 5, 25);
+		Item item = createAndUpdate(GildedRose.BACKSTAGE_PASSES , 5, 25);
 		assertEquals(28, item.quality);
 	}
 
 	@Test
 	void backstageQualityDropsToZeroAfterConcert() {
-		Item item = createAndUpdate("Backstage passes to a TAFKAL80ETC concert", 0, 25);
+		Item item = createAndUpdate(GildedRose.BACKSTAGE_PASSES , 0, 25);
 		assertEquals(0, item.quality);
 	}
 
 	@Test
 	void agedBrieNeverExpires() {
-		Item item = createAndUpdate("Aged Brie", 0, 42);
+		Item item = createAndUpdate(GildedRose.AGED_BRIE, 0, 42);
 		assertEquals(-1, item.sellIn);
 		assertEquals(44, item.quality);
 	}
 
 	@Test
 	void backstagePassMaximumQuality() {
-		Item item = createAndUpdate("Backstage passes to a TAFKAL80ETC concert", 10, 48);
-		assertEquals(50, item.quality);
+		Item item = createAndUpdate(GildedRose.BACKSTAGE_PASSES , 10, 48);
+		assertEquals(GildedRose.MAXIMUM_QUALITY, item.quality);
 
-		item = createAndUpdate("Backstage passes to a TAFKAL80ETC concert", 10, 49);
-		assertEquals(50, item.quality);
+		item = createAndUpdate(GildedRose.BACKSTAGE_PASSES , 10, 49);
+		assertEquals(GildedRose.MAXIMUM_QUALITY, item.quality);
 
-		item = createAndUpdate("Backstage passes to a TAFKAL80ETC concert", 5, 49);
-		assertEquals(50, item.quality);
+		item = createAndUpdate(GildedRose.BACKSTAGE_PASSES , 5, 49);
+		assertEquals(GildedRose.MAXIMUM_QUALITY, item.quality);
 	}
 
 	@Test
@@ -108,13 +108,13 @@ class GildedRoseTest {
 		Item item = createAndUpdate("foo", -1, 1);
 		assertEquals(0, item.quality);
 		
-		item = createAndUpdate("Sulfuras, Hand of Ragnaros", -1, 1);
+		item = createAndUpdate(GildedRose.SULFURAS, -1, 1);
 		assertEquals(1, item.quality);
 	}
 
 	@Test
 	void agedBrieMaximumQuality() {
-		Item item = createAndUpdate("Aged Brie", -1, 49);
-		assertEquals(50, item.quality);
+		Item item = createAndUpdate(GildedRose.AGED_BRIE, -1, 49);
+		assertEquals(GildedRose.MAXIMUM_QUALITY, item.quality);
 	}
 }
